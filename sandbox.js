9fd7ccbbce33080324fc039a2b30884e51b81f36
Benji Marinacci <bcmarinacci@icloud.com>
2016-03-13 05:07:45 -0700

    Add backticks

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-// Call log() to print your output to the window.
+// Call `log()` to print your output to the window.
 
 // “Let us go forth, the tellers of tales, and seize
 // whatever prey the heart longs for, and have no fear."
