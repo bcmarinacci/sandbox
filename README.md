4ec480f59e81fdfe9c64671432b205207edea553
Benji Marinacci <bcmarinacci@icloud.com>
2016-11-13 23:28:05 -0800

    Remove space, edit grammar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # A JavaScript Sandbox
 
-- Call `log( )` to print the output to the window.
+- Call `log()` to print the output in the window.
 - The theme is [Monokai](http://www.colourlovers.com/palette/1718713/Monokai).
--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,7 @@
-// Call `log()` to print your output to the window.
+// Call `log()` to print the output in the window.
 
 // “Let us go forth, the tellers of tales, and seize
 // whatever prey the heart longs for, and have no fear."
 // - W.B. Yeats
+
 'use strict';
